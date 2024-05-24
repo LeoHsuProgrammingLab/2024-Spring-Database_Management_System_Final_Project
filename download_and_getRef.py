e3ed5b79ced6c454bfe80b67b42697de5fa36866
Glen <73220388+Glen-Chen-Blue@users.noreply.github.com>
2024-05-24 22:04:07 +0800

    add finding author

--- download_and_getRef.py
+++ download_and_getRef.py
@@ -3,6 +3,29 @@ import tarfile
 import os
 import re
 
+
+replacements = {
+    r"\'{a}": 'a', r"\'{e}": 'e', r"\'{o}": 'o', r"\'{u}": 'u', r"\'{i}": 'i', r"\'{c}": 'c', r"\'{n}": 'n', r"\'{y}": 'y',
+    r"\'{A}": 'A', r"\'{E}": 'E', r"\'{O}": 'O', r"\'{U}": 'U', r"\'{I}": 'I', r"\'{N}": 'N', r"\'{Y}": 'Y',
+    r"{\'a}": 'a', r"{\'e}": 'e', r"{\'o}": 'o', r"{\'u}": 'u', r"{\'i}": 'i', r"{\'c}": 'c', r"{\'n}": 'n', r"{\'y}": 'y',
+    r"{\'A}": 'A', r"{\'E}": 'E', r"{\'O}": 'O', r"{\'U}": 'U', r"{\'I}": 'I', r"{\'N}": 'N', r"{\'Y}": 'Y',
+    r"\={e}": 'e', r"\={E}": 'E', r"\={o}": 'o', r"\={O}": 'O', r"\={a}": 'a', r"\={A}": 'A', r"\={u}": 'u', r"\={U}": 'U', r"\={i}": 'i', r"\={I}": 'I', r"\={n}": 'n', r"\={N}": 'N', r"\={y}": 'y', r"\={Y}": 'Y',
+    r"{\"{a}}": 'a', r"{\"{e}}": 'e', r"{\"{o}}": 'o', r"{\"{u}}": 'u', r"{\"{i}}": 'i', r"{\"{c}}": 'c', r"{\"{n}}": 'n', r"{\"{y}}": 'y',
+    r"{\"{A}}": 'A', r"{\"{E}}": 'E', r"{\"{O}}": 'O', r"{\"{U}}": 'U', r"{\"{I}}": 'I', r"{\"{N}}": 'N', r"{\"{Y}}": 'Y',
+    r"{\.a}": 'a', r"{\.e}": 'e', r"{\.o}": 'o', r"{\.u}": 'u', r"{\.i}": 'i', r"{\.n}": 'n', r"{\.y}": 'y',
+    r"{\.A}": 'A', r"{\.E}": 'E', r"{\.O}": 'O', r"{\.U}": 'U', r"{\.I}": 'I', r"{\.N}": 'N', r"{\.Y}": 'Y',
+    r"\v{c}": 'c', r"\v{C}": 'C', r"\v{e}": 'e', r"\v{E}": 'E', r"\v{s}": 's', r"\v{S}": 'S', r"\v{z}": 'z', r"\v{Z}": 'Z',
+    r"\"{a}": 'a', r"\"{e}": 'e', r"\"{o}": 'o', r"\"{u}": 'u', r"\"{i}": 'i', r"\"{c}": 'c', r"\"{n}": 'n', r"\"{y}": 'y',
+    r"\"{A}": 'A', r"\"{E}": 'E', r"\"{O}": 'O', r"\"{U}": 'U', r"\"{I}": 'I', r"\"{N}": 'N', r"\"{Y}": 'Y',
+    r"{\'{\a}}": 'a', r"{\'{\e}}": 'e', r"{\'{\o}}": 'o', r"{\'{\u}}": 'u', r"{\'{\i}}": 'i', r"{\'{\c}}": 'c', r"{\'{\n}}": 'n', r"{\'{\y}}": 'y',
+    r"{\'{\A}}": 'A', r"{\'{\E}}": 'E', r"{\'{\O}}": 'O', r"{\'{\U}}": 'U', r"{\'{\I}}": 'I', r"{\'{\N}}": 'N', r"{\'{\Y}}": 'Y', 
+    r"{\textquoteright}": "'", r"{\textquotedbl}": '"', r"{\textasciigrave}": "`", r"{\textasciicircum}": "^", r"{\textasciitilde}": "~", r"{\textbackslash}": "\\", r"{\textbar}": "|", r"{\textless}": "<", r"{\textgreater}": ">", r"{\textunderscore}": "_", r"{\textbraceleft}": "{", r"{\textbraceright}": "}", r"{\textbackslash}": "\\",
+    "\n": "", "\em": "", "~": " ", "\\&" : "&", "\\$" : "$", "\\%" : "%", "\\#" : "#", "\\_" : "_", "\\{" : "{", "\\}" : "}", "\\textbackslash" : "\\", "\\textasciitilde" : "~", "\\textasciicircum" : "^", "\\textasciigrave" : "`", "\\textquoteright" : "'", "\\textquotedbl" : '"',
+    r"{-}": "-",
+
+
+}
+
 def download_file(url, save_path):
     response = requests.get(url)
     with open(save_path, 'wb') as file:
@@ -33,29 +56,52 @@ def find_bbl_files(folder_path):
 
 def find_bib_refs(files):
     ref = []
+    title = ''
+    authors = []
     print("bib files: ",files)
     if len(files) > 0:
         for file in files:
             with open(file, 'r') as file:
                 data = file.read()
-            
-            lines = re.split(r',\s*\n', data)
-            # lines = data.splitlines()
-            for line in lines:
-                line = line.strip()
-                if re.match(r'^title\s*=', line):
-                    cleaned_line = re.sub(r'^title\s*=\s*', '', line)
-                    cleaned_line = re.sub(r',$', '', cleaned_line)
-                    while True:
-                        cleaned_line, count1 = re.subn(r'^\{(.*)\}$', r'\1', cleaned_line)
-                        cleaned_line, count2 = re.subn(r'^"(.*)"$', r'\1', cleaned_line)
-                        if count1 + count2 == 0:
-                            break
-                    cleaned_line = cleaned_line.replace('{\\textquoteright}', '\'')
-                    cleaned_line = cleaned_line.replace('{', '').replace('}', '').replace('\n', '')
-                    cleaned_line = re.sub(r'\s+', ' ', cleaned_line)
-                    if cleaned_line not in ref:
-                        ref.append(cleaned_line)
+            Refs = re.split(r'\n@', data)
+            for Ref in Refs:
+                Slices = re.split(r',\s*\n', Ref)
+                for Slice in Slices:
+                    Slice = Slice.strip()
+                    if re.match(r'^\s*title\s*=', Slice):
+                        cleaned_line = re.sub(r'^title\s*=\s*', '', Slice)
+                        cleaned_line = re.sub(r',$', '', cleaned_line)
+                        while True:
+                            cleaned_line, count1 = re.subn(r'^\{(.*)\}$', r'\1', cleaned_line)
+                            cleaned_line, count2 = re.subn(r'^"(.*)"$', r'\1', cleaned_line)
+                            if count1 + count2 == 0:
+                                break
+                        for old, new in replacements.items():
+                            cleaned_line = cleaned_line.replace(old, new)
+                        cleaned_line = cleaned_line.replace('{', '').replace('}', '')
+                        cleaned_line = re.sub(r'\s+', ' ', cleaned_line)
+                        title = cleaned_line
+                    elif re.match(r'^\s*author\s*=', Slice):
+                        cleaned_line = re.sub(r'^author\s*=\s*', '', Slice)
+                        cleaned_line = re.sub(r',$', '', cleaned_line)
+                        while True:
+                            cleaned_line, count1 = re.subn(r'^\{(.*)\}$', r'\1', cleaned_line)
+                            cleaned_line, count2 = re.subn(r'^"(.*)"$', r'\1', cleaned_line)
+                            if count1 + count2 == 0:
+                                break
+                        for old, new in replacements.items():
+                            cleaned_line = cleaned_line.replace(old, new)
+                        cleaned_line = cleaned_line.replace('{', '').replace('}', '')
+                        cleaned_line = re.sub(r'\s+', ' ', cleaned_line)
+                        authors = cleaned_line
+                        authors = authors.split(' and ')
+                        for i in range(len(authors)):
+                            if ',' in authors[i]:
+                                authors[i] = authors[i].split(', ')[1] + ' ' + authors[i].split(', ')[0]
+                if title != '' and authors != '' and {'title': title, 'authors': authors} not in ref:
+                    ref.append({'title': title, 'authors': authors})
+                    title = ''
+                    authors = []
     
     return ref
 
@@ -71,32 +117,50 @@ def find_bbl_refs(files):
             for item in bbl_items[1:]:
                 newblocks = re.split(r'\\newblock', item)
                 if len(newblocks) > 2:
-                    bbl_content = newblocks[1].strip()
-                    cleaned_line = bbl_content.replace('{', '').replace('}', '').replace('\n', '').replace('\\em', '')
+                    cleaned_line = newblocks[1].strip()
+                    for old, new in replacements.items():
+                            cleaned_line = cleaned_line.replace(old, new)
+                    cleaned_line = cleaned_line.replace('{', '').replace('}', '')
                     cleaned_line = re.sub(r'.$', '', cleaned_line)
                     cleaned_line = cleaned_line.strip()
-                    if cleaned_line not in ref:
-                        ref.append(cleaned_line)
-    
+                    title = cleaned_line
+                    
+                    bbl_author = newblocks[0].strip()
+                    pos = bbl_author.find(']')
+                    if pos != -1:
+                        bbl_author = bbl_author[pos+1:]
+                    pos = bbl_author.find('}')
+                    if pos != -1:
+                        cleaned_line = bbl_author[pos+1:]
+                    for old, new in replacements.items():
+                        cleaned_line = cleaned_line.replace(old, new)
+                    cleaned_line = cleaned_line.replace('{', '').replace('}', '')
+                    cleaned_line = re.sub(r'.$', '', cleaned_line)
+                    authors = re.split(r',\s', cleaned_line)
+                    for i in range(len(authors)):
+                        authors[i] = authors[i].strip().lstrip("and ").strip()
+                    if title != '' and authors != '' and {'title': title, 'authors': authors} not in ref:
+                        ref.append({'title': title, 'authors': authors})
+                        title = ''
+                        authors = []
     return ref
 
 if __name__ == "__main__":
 
     target=input("input the paper index: ").strip()
-    # ex: https://arxiv.org/abs/2405.12117
 
     url = f"https://arxiv.org/src/{target}"
     url2= f"https://arxiv.org/pdf/{target}"
-    tar_gz_path = f"./paper/{target}.tar.gz"
+    download_path = f"./paper/{target}.tar.gz"
     pdf_path = f"./paper/{target}/{target}.pdf"
     save_path = f"./paper/{target}"
     ref=[]
 
     if not os.path.exists(save_path):    
-        download_file(url, tar_gz_path)
-        unzip_tar_gz(tar_gz_path, save_path)
+        download_file(url, download_path)
+        unzip_tar_gz(download_path, save_path)
         download_file(url2, pdf_path)
-        remove_file(tar_gz_path)
+        remove_file(download_path)
         
     files1 = find_bib_files(save_path)
     files2 = find_bbl_files(save_path)
