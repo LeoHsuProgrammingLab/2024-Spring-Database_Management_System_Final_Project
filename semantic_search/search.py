75f0b9c0560907079c10e2ceefae4950acc477b9
Leo Hsu <leodream251324@gmail.com>
2024-06-12 02:21:54 +0800

    search using vector index

--- semantic_search/search.py
+++ semantic_search/search.py
@@ -4,10 +4,9 @@ from db_helper import Neo4j_interface
 
 if __name__ == "__main__":
     interface = Neo4j_interface(conn_info='../credential.txt')
-    text = input("Please input what you want to search: ")
-    k_papers = interface.get_k_similar_papers(text, 5)
-    titles = [title for title, _ in k_papers]
-    scores = [score for _, score in k_papers]
-
-    for title, score in zip(titles, scores):
-        print(title, round(score, 2))
+    title = 'Convolutional LSTM Network: A Machine Learning Approach for Precipitation Nowcasting'
+    # result = interface.exec_query('DROP INDEX embedding')
+    # result = interface.exec_query('SHOW VECTOR INDEXES')
+    # interface.create_vector_index()
+    result = interface.search_k_similar(title, k=5)
+    print(result)
